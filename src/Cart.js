9fff76d3d068614a4fdb1cb5518a9839a1be2fa0
RohanHarichandan <harichandanrohan24@gmail.com>
2022-08-14 01:37:59 +0530

    add props to cartItem

--- src/Cart.js
+++ src/Cart.js
@@ -4,12 +4,51 @@ import Cartitem from './CartItem';
 
 class Cart extends  React.Component{
 
+  constructor(){
+    super();
+  this.state={
+   products:[
+    {
+      price:99,
+      title:'Watch',
+       qty:1,
+       img:'',
+       id:1
+    },{
+      price:999,
+      title:'Mobile Phone',
+       qty:10,
+       img:'',
+       id:2
+    },{
+      price:999,
+      title:'Laptop',
+       qty:4,
+       img:'',
+       id:3
+    }
+   ]
+
+  }
+//   this.increaseQuantity=this.increaseQuantity.bind(this);
+// this.testing(); 
+}
+
   render(){
+    const {products}=this.state;
     return(
     <div className='cart'>
-      <Cartitem/>
-      <Cartitem/>
-      <Cartitem/>
+    
+      {products.map((product)=>{
+         return (<Cartitem 
+           product={product}
+            key={product.id}
+            
+           
+            />)
+      })
+
+      }
     </div>
     );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -3,17 +3,7 @@ import React from 'react';
 
 class Cartitem extends React.Component{
 
-        constructor(){
-            super();
-          this.state={
-            price:999,
-            title:'Mobile Phone',
-             qty:1,
-             img:''
-          }
-        //   this.increaseQuantity=this.increaseQuantity.bind(this);
-        // this.testing(); 
-        }
+       
           //  testing(){
           //   const promise=new Promise((resolve,reject)=>{
           //       setTimeout(()=>{
@@ -59,10 +49,12 @@ class Cartitem extends React.Component{
             );
         }
      render(){
-        const {price,title,qty}=this.state;
+      console.log('this.props',this.props);
+        const {price,title,qty}=this.props.product;
         return(
            
         <div className="cart-item">
+          {this.props.jsx}
           <div className='left-block'>
            <img style={styles.image}/>
           </div>
