87ae770ad1fb44942b8399d142dbffc512d906cf
RohanHarichandan <harichandanrohan24@gmail.com>
2022-08-17 02:12:10 +0530

    add total price

--- src/App.js
+++ src/App.js
@@ -12,19 +12,19 @@ class App extends React.Component {
       price:99,
       title:'Watch',
        qty:1,
-       img:'',
+       img:'https://media.istockphoto.com/photos/wristwatch-picture-id1359180038?b=1&k=20&m=1359180038&s=170667a&w=0&h=DdYIpG4JNxkI4UZX_HR1GI8uUzLcUef1-JkvYgY5yG0=',
        id:1
     },{
       price:999,
       title:'Mobile Phone',
        qty:10,
-       img:'',
+       img:'https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
        id:2
     },{
       price:999,
       title:'Laptop',
        qty:4,
-       img:'',
+       img:'https://images.unsplash.com/photo-1611078489935-0cb964de46d6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGFwdG9wc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60',
        id:3
     }
    ]
@@ -75,6 +75,17 @@ handleDecreaseQuantity=(product)=>{
       });
      return count;
    }
+   
+    getCartTotal=()=>{
+      const {products}=this.state;
+        let cartTotal=0;
+        products.map((product)=>{
+          cartTotal+=cartTotal+product.qty*product.price
+        })
+        return cartTotal;
+    }
+
+
 
   render(){
     const {products}=this.state;
@@ -88,6 +99,8 @@ handleDecreaseQuantity=(product)=>{
       onDecreaseQuantity={this.handleDecreaseQuantity}
       onDeleteProduct={this.handleDeleteProduct}
      />
+
+     <div style={{padding:10,fontSize:20}}>TOTAL : {this.getCartTotal()}</div>
     </div>
   );
   }
--- src/CartItem.js
+++ src/CartItem.js
@@ -17,7 +17,7 @@ import React from 'react';
            
         <div className="cart-item">
           <div className='left-block'>
-           <img style={styles.image}/>
+           <img style={styles.image} src={product.img}/>
           </div>
           <div className='right-block'>
                <div style={{fontSize:25}}>{title}</div>
